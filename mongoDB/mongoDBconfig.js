5c4e9e7c06f0fb3c6c0d2d5515d2d3227ef1a592
Ramadoss3427 <ramadoss3427@gmail.com>
2024-03-23 16:13:09 +0530

    task complete

--- mongoDB/mongoDBconfig.js
+++ mongoDB/mongoDBconfig.js
@@ -7,7 +7,7 @@ const MONGODB_URL = process.env.MONGODB_URL;
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URL, {
-       
+     
     });
    console.log('MongoDB connected');
   } catch (error) {
